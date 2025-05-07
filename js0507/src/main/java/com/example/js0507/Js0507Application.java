43286b88abc0d04c69424f67488f87062320a18a
okonys <62321546+ysk1007@users.noreply.github.com>
2025-05-07 14:03:23 +0900

    forEach, map, filter, setTimeout, setInterval
    forEach,map,filter(value,index,array)를 활용해서 반복문을 사용할 수 있다.
    일정 시간 뒤에 -> setTimeout({함수},{시간})
    일정 시간 마다 -> setInterval({함수},{시간})

---
+++ js0507/src/main/java/com/example/js0507/Js0507Application.java
@@ -0,0 +1,13 @@
+package com.example.js0507;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Js0507Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Js0507Application.class, args);
+	}
+
+}
---
+++ js0507/src/test/java/com/example/js0507/Js0507ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.js0507;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Js0507ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

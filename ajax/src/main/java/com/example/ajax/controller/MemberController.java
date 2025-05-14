35ec3f5f512a8a8f9ea946f80bff161b400c56c1
okonys <62321546+ysk1007@users.noreply.github.com>
2025-05-14 10:06:16 +0900

    카카오 우편번호 API 사용하기
    기존 프로젝트에 카카오 우편번호 외부 api를 가져와 사용함.
    주소를 데이터 베이스에 저장함.

--- ajax/src/main/java/com/example/ajax/controller/MemberController.java
+++ ajax/src/main/java/com/example/ajax/controller/MemberController.java
@@ -22,12 +22,14 @@ public class MemberController {
 	@Transactional
 	@PostMapping("/joinMember")
 	public String joinMember(Member member) {
+		member.setAddress();
 		int row = memberMapper.insertMember(member);
 		
 		// insert 행의 수가 1이 아님
 		if(row != 1) {
 			throw new RuntimeException();
 		}
+		
 		return "redirect:/joinMember";
 	}
 }
--- ajax/src/main/java/com/example/ajax/dto/Member.java
+++ ajax/src/main/java/com/example/ajax/dto/Member.java
@@ -8,4 +8,20 @@ public class Member {
 	private String pw;
 	private String gender;
 	private int age;
+	
+	
+	private String address;
+	
+	private String postcode;
+	private String roadAddress;
+	private String jibunAddress;
+	private String detailAddress;
+	private String extraAddress;
+	
+	
+	public void setAddress() { 
+		 this.address = 
+				 this.postcode + " " + this.roadAddress + " " + this.jibunAddress + " " 
+				+ this.detailAddress + " " + this.extraAddress;
+	}
 }
